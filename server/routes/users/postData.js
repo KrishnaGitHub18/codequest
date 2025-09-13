d92962722f9dd11218aafa38807bb6fcb28675c7
KrishnaGitHub18 <krishndeheriya1803@gmail.com>
2025-09-14 01:01:22 +0530

    token will be created by signup route

--- server/routes/users/postData.js
+++ server/routes/users/postData.js
@@ -1,6 +1,9 @@
 import express from "express";
 import User from "../../models/userModel.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 
 const router = express.Router();
 
@@ -10,7 +13,7 @@ router.post("/signup", async (req, res) => {
     if (!name || !mailId || !password) {
       return res.status(400).json({ error: "All fields are required" });
     }
-    
+
     const existingUser = await User.findOne({ mailId });
     if (existingUser) {
       return res.status(409).json({ error: "User already exists" });
@@ -18,13 +21,25 @@ router.post("/signup", async (req, res) => {
 
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
+
     const newUser = new User({ name, mailId, password: hashedPassword });
     await newUser.save();
 
+    const jwtSecret = process.env.JWTSECRET;
+    if (!jwtSecret) {
+      throw new Error("JWTSECRET is not defined in environment");
+    }
+
+    const token = jwt.sign(
+      { id: newUser._id, mailId: newUser.mailId },
+      jwtSecret,
+      { expiresIn: "7d" } 
+    );
 
     res.status(201).json({
       message: "User registered successfully",
-      user: { id: newUser._id, name: newUser.name, mailId: newUser.mailId }
+      token,
+      user: { id: newUser._id, name: newUser.name, mailId: newUser.mailId },
     });
 
   } catch (err) {
